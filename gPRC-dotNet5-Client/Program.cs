ae082ddad51b78878c7258ba681e87b8160f997e
Claudio V <claudio@iplatform.com>
2020-12-09 14:55:57 +0200

    Initial Commit

---
+++ gPRC-dotNet5-Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using gRPC_dotNet5;
+using System;
+using System.Threading.Tasks;
+
+namespace gPRC_dotNet5_Client
+{
+    class Program
+    {
+       
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(
+                  new HelloRequest
+                  {
+                      Name = ".NET 5 - grpcClient"
+                  });
+            Console.WriteLine("From Server: " + response.Message);
+            Console.ReadKey();
+        }
+    }
+}
